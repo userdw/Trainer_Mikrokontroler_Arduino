5298d5c3470de07fed080809db3ac463c52d93d0
userdw <insidetrainingie@gmail.com>
2022-12-13 10:13:45 +0700

    Add files via upload

---
+++ B_GPIO sebagai Digital Input/02_Membaca Data Digital Menggunakan Tactile Switch/input_pullup.ino
@@ -0,0 +1,13 @@
+#define pinPushButton 4
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pinPushButton, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool baca_pb = digitalRead(pinPushButton);
+  Serial.println(baca_pb); 
+}
