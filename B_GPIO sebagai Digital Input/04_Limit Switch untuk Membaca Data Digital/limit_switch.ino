7255244c96ced72bd44afd3aa20af627e2f32a61
userdw <insidetrainingie@gmail.com>
2022-12-14 15:02:56 +0700

    Add files via upload

---
+++ B_GPIO sebagai Digital Input/04_Limit Switch untuk Membaca Data Digital/limit_switch.ino
@@ -0,0 +1,13 @@
+#define pinLimitSwitch 4
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pinLimitSwitch, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool bacaPin_LS = digitalRead(pinLimitSwitch);
+  Serial.println(bacaPin_LS);
+}
