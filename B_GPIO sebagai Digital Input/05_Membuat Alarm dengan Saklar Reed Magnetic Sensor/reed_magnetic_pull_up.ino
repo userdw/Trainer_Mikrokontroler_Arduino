c567d42594668896bb73a1689a37a11f4892233c
userdw <insidetrainingie@gmail.com>
2022-12-15 10:57:34 +0700

    Add files via upload

---
+++ B_GPIO sebagai Digital Input/05_Membuat Alarm dengan Saklar Reed Magnetic Sensor/reed_magnetic_pull_up.ino
@@ -0,0 +1,13 @@
+#define pinReedMagnetic 4
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pinReedMagnetic, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool baca_ReedMagnetic = digitalRead(pinReedMagnetic);
+  Serial.println(baca_ReedMagnetic);
+}
