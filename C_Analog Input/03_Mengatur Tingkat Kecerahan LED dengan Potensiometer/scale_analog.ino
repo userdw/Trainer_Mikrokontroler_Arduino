a06ae0f53faa1827c7269122e860ce05e4121367
userdw <insidetrainingie@gmail.com>
2022-12-19 14:59:15 +0700

    Add files via upload

---
+++ C_Analog Input/03_Mengatur Tingkat Kecerahan LED dengan Potensiometer/scale_analog.ino
@@ -0,0 +1,20 @@
+#define pinPotensiometer A0
+int bacaDataAnalog = 0;
+int dataScale = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bacaDataAnalog = analogRead(pinPotensiometer);
+  dataScale = map(bacaDataAnalog, 0, 1023, 0, 255);
+  Serial.print("Data Analog :");
+  Serial.print(bacaDataAnalog);
+  Serial.print("   ");
+  Serial.print("Hasil Scale :");
+  Serial.print(dataScale);
+  Serial.println("   ");
+}
