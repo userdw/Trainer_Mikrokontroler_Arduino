4485658f058ed5a473c43086bd7d73c87b7bed86
userdw <insidetrainingie@gmail.com>
2022-12-21 11:00:28 +0700

    Add files via upload

---
+++ D_Belajar Menggunakan Sensor/02_Mengukur Tingkat Intensitas Cahaya dengan LDR/baca_analog_LDR.ino
@@ -0,0 +1,12 @@
+#define pinLDR  A0
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int bacaLDR = analogRead(pinLDR);
+  Serial.println(bacaLDR); 
+}
