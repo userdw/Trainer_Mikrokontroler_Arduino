6edab187c1f7efe7d70269c22701fe0b11985bd6
userdw <insidetrainingie@gmail.com>
2022-12-22 09:57:19 +0700

    Add files via upload

---
+++ D_Belajar Menggunakan Sensor/03_Menghitung Jarak Benda menggunakan Sensor Ultrasonik HC-SR04/sensor_ultrasonik_hcsr04.ino
@@ -0,0 +1,24 @@
+#define pinEcho 2 
+#define pinTrig 3 
+
+long durasi; 
+int jarak; 
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(pinTrig, OUTPUT); 
+  pinMode(pinEcho, INPUT); 
+}
+void loop() {
+  digitalWrite(pinTrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrig, LOW);
+  durasi = pulseIn(pinEcho, HIGH);
+  jarak = durasi * 0.034 / 2;
+  Serial.print("Jarak: ");
+  Serial.print(jarak);
+  Serial.println(" cm");
+  delay(100);
+}
